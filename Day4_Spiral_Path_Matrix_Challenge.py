5c7f872b3e746e239ac455250a45d419dd6d38dc
Manikanta-009 <n.manikanta733@gmail.com>
2023-09-04 12:40:53 +0530

    Day 4: Spiral Path Matrix Challenge

--- Day4_Spiral_Path_Matrix_Challenge.py
+++ Day4_Spiral_Path_Matrix_Challenge.py
@@ -35,33 +35,41 @@ Note:
 """
 ### Solution Code
 
-def spiralPathMatrix(matrix, row_count, coloumn_count):
+def spiralPathMatrix(matrix, row_count, column_count):
+    # Initialize variables to keep track of the current row and column boundaries
     row_begin, row_end = 0, row_count - 1
-    column_begin, column_end = 0, coloumn_count - 1
-    result = []
+    column_begin, column_end = 0, column_count - 1
+    result = []  # Initialize an empty list to store the spiral path elements
 
+    # Loop until we have covered all rows and columns
     while row_begin <= row_end and column_begin <= column_end:
+        # Traverse from left to right along the current row
         for index in range(column_begin, column_end + 1):
             result.append(matrix[row_begin][index])
-        row_begin += 1
+        row_begin += 1  # Move the row boundary down
 
+        # Traverse from top to bottom along the current column
         for index in range(row_begin, row_end + 1):
             result.append(matrix[index][column_end])
-        column_end -= 1
+        column_end -= 1  # Move the column boundary left
 
+        # Check if there are more rows to traverse
         if row_begin <= row_end:
+            # Traverse from right to left along the current row
             for index in range(column_end, column_begin - 1, -1):
                 result.append(matrix[row_end][index])
-        row_end -= 1
+            row_end -= 1  # Move the row boundary up
 
+        # Check if there are more columns to traverse
         if column_begin <= column_end:
+            # Traverse from bottom to top along the current column
             for index in range(row_end, row_begin - 1, -1):
                 result.append(matrix[index][column_begin])
-        column_begin += 1
+            column_begin += 1  # Move the column boundary right
 
     return result
 
-
+# Define the input matrix
 matrix = [
     [1, 2, 3, 4, 5],
     [6, 7, 8, 9, 10],
@@ -71,15 +79,21 @@ matrix = [
     [26, 27, 28, 29, 30]
 ]
 
+# Get the number of rows and columns in the matrix
 row, column = len(matrix), len(matrix[0])
+
+# Call the spiralPathMatrix function to obtain the result
 result = spiralPathMatrix(matrix, row, column)
 
+# Print the input matrix
 print("Input Matrix:")
 for row in matrix:
     print(row)
-    
+
+# Print the elements in spiral order
 print("\nSpiral Path Result:")
 print(result)
+
 
 """
 Connect with me on 
